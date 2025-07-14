445d4b9da3167597a028065b93eb0c82e40d1929
Robin <robinnem@outlook.com>
2025-07-14 09:43:58 -0400

    JavaScript Practice

--- script.js
+++ script.js
@@ -58,3 +58,55 @@ let fruits = ["apple", "banana", "cherry"];
 for (let i = 0; i < fruits.length; i++) {
   console.log("Fruit: " + fruits[i]);
 }
+
+let student = {
+  name: "John",
+  age: 20,
+  major: "Computer Science",
+  email: "john123@gmail.com",
+  isEnrolled: true,
+  modulesFinished: [0, 1, 2, 3],
+};
+
+let module1 = {
+  number: 0,
+  title: "Introduction to Programming",
+  description: "Learn the basics of programming using JavaScript.",
+};
+
+let module2 = {
+  number: 1,
+  title: "Data Structures",
+  description: "Explore different data structures and their applications.",
+};
+
+let module3 = {
+  number: 2,
+  title: "Web Development",
+  description:
+    "Learn how to build web applications using HTML, CSS, and JavaScript.",
+};
+
+console.log("Student Name: " + student.name);
+console.log("Student Age: " + student.age);
+
+let modulesArray = [
+  {
+    number: 0,
+    title: "Introduction to Programming",
+    description: "Learn the basics of programming using JavaScript.",
+  },
+  {
+    number: 1,
+    title: "Data Structures",
+    description: "Explore different data structures and their applications.",
+  },
+  {
+    number: 2,
+    title: "Web Development",
+    description:
+      "Learn how to build web applications using HTML, CSS, and JavaScript.",
+  },
+];
+
+console.log(modulesArray[1].title);
