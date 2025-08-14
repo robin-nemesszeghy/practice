fbf2fef5c6501a757f95b00c15d3e78681b05c6e
Robin <robinnem@outlook.com>
2025-08-14 05:59:29 -0400

    JavaScript Practice with Type Conversion

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
-let age = 18;
-let height = 160;
-let has_adult = false;
+// Values to convert
+const stringValue = '42';
+const zeroNumber = 0;
+const positiveNumber = 7;
+const emptyString = '';
+const boolValue = true;
+const numValue = 123;
 
-// Write your code below
-if (age < 12) {
-  console.log("Sorry, you're too young");
-} else if (height < 150) {
-  console.log("Sorry, you're not tall enough");
-} else if (age < 15) {
-  if (!has_adult) {
-    console.log("Sorry, you need an adult with you");
-  } else {
-    console.log("You can ride with adult supervision!");
-  }
-} else {
-  console.log("You can ride by yourself!");
-}
+// TODO: Convert stringValue to a boolean and log the result
+let bool1 = Boolean(stringValue);
+console.log("Boolean('42'): ", bool1);
 
-let length = 5;
-let width = 3.2;
+// TODO: Convert zeroNumber to a boolean and log the result
+let bool2 = Boolean(zeroNumber);
+console.log("Boolean(0): ", bool2);
 
-// Write code below
-let lengthFloat = parseFloat(length);
-let widthFloat = parseFloat(width);
+// TODO: Convert positiveNumber to a boolean and log the result
+let bool3 = Boolean(positiveNumber);
+console.log("Boolean(7): ", bool3);
 
-let area = lengthFloat * widthFloat;
-let perimeter = 2 * lengthFloat + 2 * widthFloat;
-console.log(`The area is: ${area} and the perimeter is: ${perimeter}`);
+// TODO: Convert emptyString to a boolean and log the result
+let bool4 = Boolean(emptyString);
+console.log("Boolean(''): ", bool4);
+
+// TODO: Convert boolValue to a string and log the result
+let str1 = String(boolValue);
+console.log("String(true): ", str1);
+
+// TODO: Convert numValue to a string and log the result
+let str2 = String(numValue);
+console.log("String(123): ", str2);
