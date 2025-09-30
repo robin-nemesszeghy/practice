ee42e7056005f16f4caf4b19ac6f45720273df7b
Robin <robinnem@outlook.com>
2025-09-29 20:42:05 -0400

    Functions, Arrays, and Objects Practice

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-let grades = ["A+", "A", "FAIL"];
-let newGrades = [];
+let users = [];
 
-for (let i = 0; i < grades.length; i++) {
-  if (grades[i] !== "FAIL") {
-    newGrades.push(grades[i]);
-  }
+function register(user) {
+  let user = {
+    username: username,
+    email: email,
+    password: password,
+    subscriptionStatus: subscriptionStatus,
+    discordId: discordId,
+    lessonsCompleted: lessonsCompleted,
+  };
+  users.push(user);
 }
 
-console.log(newGrades);
+register({
+  username: "john_doe",
+  email: "john@example.com",
+  password: "password123",
+  subscriptionStatus: "premium",
+  discordId: "123456789",
+  lessonsCompleted: [0, 1],
+});
+
+console.log(users);
