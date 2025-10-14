f74dfdd1680c1f60d0df5ec2d108246bb7e9ee49
Robin <robinnem@outlook.com>
2025-10-14 11:59:26 -0400

    JavaScript DOM Practice

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-let users = [];
+// // First way of accessing an element (in the DOM)
+// console.log(document.querySelector("#title"));
 
-function register(user) {
-  let user = {
-    username: username,
-    email: email,
-    password: password,
-    subscriptionStatus: subscriptionStatus,
-    discordId: discordId,
-    lessonsCompleted: lessonsCompleted,
-  };
-  users.push(user);
+// // Second way of accessing an element (in the DOM)
+// console.log(document.getElementById("title"));
+
+// // Change HTML of any element (in the DOM)
+// document.querySelector("#title").innerHTML += " Frontend Simplified";
+
+// // Change CSS of any element (in the DOM)
+// // document.querySelector("#title").style.color = "blue";
+// // document.querySelector("#title").style.fontSize = "20px";
+
+// Function that changes title color to red (in the DOM)
+function changeTitleToRed() {
+  document.getElementById("title").style.color = "red";
 }
 
-register({
-  username: "john_doe",
-  email: "john@example.com",
-  password: "password123",
-  subscriptionStatus: "premium",
-  discordId: "123456789",
-  lessonsCompleted: [0, 1],
-});
-
-console.log(users);
+function toggleDarkMode() {
+  document.querySelector("body").classList.toggle("dark-theme");
+}
