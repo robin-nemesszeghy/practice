024d77c4e9debbb7c9f297dda9e4a437778acc06
Robin <robinnem@outlook.com>
2025-10-16 16:16:45 -0400

    Async/Await Promise Practice

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 // console.log(fetch("https://jsonplaceholder.typicode.com/users/1"));
+const emailRef = document.querySelector(".email");
 
 // 1. Then
-fetch("https://jsonplaceholder.typicode.com/users/1").then((response) => {
-  response.json().then((data) => {
-    console.log(data);
-  });
-});
+// fetch("https://jsonplaceholder.typicode.com/users/1")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//     emailRef.innerHTML = data.email;
+//   });
 
 // 2. Async/Await
+async function main() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
+  const data = await response.json();
+  console.log(data);
+  emailRef.innerHTML = data.email;
+}
+
+main();
