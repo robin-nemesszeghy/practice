fa7075c18507049492fea9b5043c051abbc824db
Robin <robinnem@outlook.com>
2025-10-16 15:03:36 -0400

    JavaScript Promise Practice

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
-// // First way of accessing an element (in the DOM)
-// console.log(document.querySelector("#title"));
+// console.log(fetch("https://jsonplaceholder.typicode.com/users/1"));
 
-// // Second way of accessing an element (in the DOM)
-// console.log(document.getElementById("title"));
+// 1. Then
+fetch("https://jsonplaceholder.typicode.com/users/1").then((response) => {
+  response.json().then((data) => {
+    console.log(data);
+  });
+});
 
-// // Change HTML of any element (in the DOM)
-// document.querySelector("#title").innerHTML += " Frontend Simplified";
-
-// // Change CSS of any element (in the DOM)
-// // document.querySelector("#title").style.color = "blue";
-// // document.querySelector("#title").style.fontSize = "20px";
-
-// Function that changes title color to red (in the DOM)
-function changeTitleToRed() {
-  document.getElementById("title").style.color = "red";
-}
-
-function toggleDarkMode() {
-  document.querySelector("body").classList.toggle("dark-theme");
-}
+// 2. Async/Await
