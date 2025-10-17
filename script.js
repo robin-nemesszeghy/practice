7ad025c2c56a189c374fb05789b2a2768b53599e
Robin <robinnem@outlook.com>
2025-10-17 11:54:55 -0400

    Creating Promises

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
-// console.log(fetch("https://jsonplaceholder.typicode.com/users/1"));
-const emailRef = document.querySelector(".email");
+const statusRef = document.querySelector(".status");
+
+function getSubscriptionStatus() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("FREE");
+    }, 2000);
+  });
+}
 
 // 1. Then
-// fetch("https://jsonplaceholder.typicode.com/users/1")
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((data) => {
-//     console.log(data);
-//     emailRef.innerHTML = data.email;
-//   });
+// getSubscriptionStatus().then((response) => console.log(response));
 
 // 2. Async/Await
 async function main() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
-  const data = await response.json();
-  console.log(data);
-  emailRef.innerHTML = data.email;
+  const status = await getSubscriptionStatus();
+  statusRef.innerHTML = status;
 }
 
 main();
+
+// Create getVideo function
+
+function getVideo(subscriptionStatus) {
+  return new Promise((resolve, reject) => {
+    if (subscriptionStatus === "VIP") {
+      resolve("show video");
+    } else if (subscriptionStatus === "FREE") {
+      resolve("show trailer");
+    } else {
+      reject("no video");
+    }
+  });
+}
+
+async function main() {
+  const status = await getSubscriptionStatus();
+  statusRef.innerHTML = status;
+  console.log(await getVideo(status));
+}
