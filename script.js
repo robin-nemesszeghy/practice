4b42de721d43967a67ceaf02aaa1a16a560b60ab
Robin <robinnem@outlook.com>
2025-10-21 16:20:07 -0400

    JavaScript Medium Challenges P1

--- script.js
+++ script.js
@@ -1,166 +1,75 @@
-// Addition Function
+// Falsy or Truthy Functions
 
-function addition(num1, num2) {
-  return num1 + num2;
+function filterOutFalsy(elem1, elem2) {
+  return !elem1 ? elem1 : elem2;
 }
 
-console.log(addition(3, 5));
+console.log(filterOutFalsy(0, 500));
+console.log(filterOutFalsy(false, 100));
+console.log(filterOutFalsy(true, "Dog"));
 
-// Hours Into Seconds Function
+// Array Length
 
-function hoursIntoSeconds(hour) {
-  return hour * 60 * 60;
+function arrLength(arr) {
+  return arr.length;
 }
 
-console.log(hoursIntoSeconds(2));
+console.log(arrLength([1, 2, 3]));
+console.log(arrLength([5, 0, -4, 1]));
+console.log(arrLength([]));
 
-// Perimeter of a Rectangle Function
+// Array Last Element
 
-function calcPerimeter(length, width) {
-  return (length + width) * 2;
+function lastElem(arr) {
+  return arr[arr.length - 1];
 }
 
-console.log(calcPerimeter(6, 7));
+console.log(lastElem([3, 2, 0, 6, 2]));
+console.log(lastElem(["dog", "cat", "ball"]));
+console.log(lastElem([null, 5, false]));
 
-// Area of a Triangle Function
+// Array Sum
 
-function calcTriangleArea(base, height) {
-  return 0.5 * base * height;
-}
-
-console.log(calcTriangleArea(3, 2));
-
-// Extend a String
-
-function appendFrontend(str) {
-  return str + "Frontend";
-}
-
-console.log(appendFrontend("Apple"));
-
-// Greater than 100 FULL
-
-function sumGreaterThan100(num1, num2) {
-  if (num1 + num2 > 100) {
-    return true;
-  } else {
-    return false;
+function arrSum(arrr) {
+  let sum = 0;
+  for (let i = 0; i < arrr.length; ++i) {
+    sum = sum + arrr[i];
   }
+  return sum;
 }
 
-console.log(sumGreaterThan100(50, 60));
+console.log(arrSum([2, 2, 2]));
+console.log(arrSum([100, 200, 500]));
+console.log(arrSum([0, -5, -10]));
 
-// Greater than 100 LESS CODE
+// Add Up Numbers from a Single Number
 
-function sumGreaterThan100(num1, num2) {
-  if (num1 + num2 > 100) {
-    return true;
+function progressiveSum(num) {
+  let sum = 0;
+  for (let i = 1; i <= num; ++i) {
+    sum = sum + i;
   }
-  return false;
+  return sum;
 }
 
-console.log(sumGreaterThan100(50, 60));
+console.log(progressiveSum(3));
+console.log(progressiveSum(4));
+console.log(progressiveSum(600));
 
-// Greater than 100 SIMPLEST
+// Calculate the Time
 
-function sumGreaterThan100(num1, num2) {
-  return num1 + num2 > 100;
-}
-
-console.log(sumGreaterThan100(50, 60));
-
-// Less than or Euqal to Zero
-
-function lessThanOrEqualToZero(num) {
-  return num <= 0;
-}
-
-console.log(lessThanOrEqualToZero(0));
-console.log(lessThanOrEqualToZero(-10));
-console.log(lessThanOrEqualToZero(8));
-console.log(lessThanOrEqualToZero(100));
-console.log(lessThanOrEqualToZero(-100));
-
-// Return Opposite Boolean
-
-function oppositeBoolean(bool) {
-  return !bool;
-}
-
-console.log(oppositeBoolean(false));
-console.log(oppositeBoolean(true));
-
-// Is Not Zero Function
-
-function isNotZero(num) {
-  return num !== 0;
-}
-
-console.log(isNotZero(0));
-console.log(isNotZero(9));
-console.log(isNotZero(null));
-console.log(isNotZero("0"));
-
-// Calculate Remainder
-
-function calcRemainder(num1, num2) {
-  return num1 % num2;
-}
-
-console.log(calcRemainder(4, 2));
-console.log(calcRemainder(7, 8));
-console.log(calcRemainder(9, 8));
-
-// Is the Number Odd?
-
-function isOdd(num) {
-  return num % 2 !== 0;
-}
-
-console.log(isOdd(1));
-console.log(isOdd(2));
-console.log(isOdd(3));
-
-// Number Even: 1, Number Odd: -1
-
-function booleanInteger(num) {
-  if (num % 2 === 0) {
-    return 1;
+function calcTime(seconds) {
+  let timerMinutes = Math.floor(seconds / 60);
+  let timerSeconds = seconds % 60;
+  if (timerMinutes.toString().length === 1) {
+    timerMinutes = "0" + timerMinutes;
   }
-  return -1;
+  if (timerSeconds.toString().length === 1) {
+    timerSeconds = "0" + timerSeconds;
+  }
+  return timerMinutes + ":" + timerSeconds;
 }
 
-console.log(booleanInteger(1));
-console.log(booleanInteger(2));
-console.log(booleanInteger(5));
-
-// Number Even: 1, Number Odd: -1 in ONE LINE using TURNARY OPERATOR
-
-function booleanIntegerT(num) {
-  return num % 2 === 0 ? 1 : -1;
-}
-
-console.log(booleanIntegerT(1));
-console.log(booleanIntegerT(2));
-console.log(booleanIntegerT(5));
-
-// Check if User is Logged In AND Subscribed
-
-function isLoggedInAndSubscribed(str1, str2) {
-  return str1 === "LOGGED_IN" && str2 === "SUBSCRIBED";
-}
-
-console.log(isLoggedInAndSubscribed("LOGGED_IN", "SUBSCRIBED"));
-console.log(isLoggedInAndSubscribed("LOGGED_IN", "UNSUBSCRIBED"));
-console.log(isLoggedInAndSubscribed("LOGGED_OUT", "SUBSCRIBED"));
-
-// Check if User is Logged In OR Subscribed
-
-function isLoggedInOrSubscribed(str1, str2) {
-  return str1 === "LOGGED_IN" || str2 === "SUBSCRIBED";
-}
-
-console.log(isLoggedInOrSubscribed("LOGGED_IN", "SUBSCRIBED"));
-console.log(isLoggedInOrSubscribed("LOGGED_IN", "UNSUBSCRIBED"));
-console.log(isLoggedInOrSubscribed("LOGGED_OUT", "SUBSCRIBED"));
-console.log(isLoggedInOrSubscribed("LOGGED_OUT", "UNSUBSCRIBED"));
+console.log(calcTime(66));
+console.log(calcTime(50));
+console.log(calcTime(300));
